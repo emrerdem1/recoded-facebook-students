556f48dc559d0cd7ec28118bcbd75be4908d1673
emrerdem1 <emrerdem94@gmail.com>
2020-06-13 15:18:40 +0300

    get data from Google sign-in and post it to db

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { db, provider } from "./firebase";
 import { Form, Col, Button } from 'react-bootstrap';
 import firebase from "firebase/app";
@@ -7,35 +7,42 @@ import "firebase/auth";
 
 
 const SignUpPage = () => {
+  const input_city = useRef();
+  const input_brief = useRef();
 
-  const signViaGoogle = () =>
+  const [userObject, setUserObject] = useState({
+    name: 'The name in Google for the account',
+    userId: 'The unique user ID (available via Google. Please dont make a user ID yourself)',
+    imageUrl: ' The Google profile picture of the image (available via Google)',
+    profile: '',
+    city: '',
+    email: ''
+  });
+
+  const signViaGoogle = () => {
     firebase.auth().signInWithPopup(provider).then(function (result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
       let genelprops = result.user;
       let { email, displayName, photoURL, uid } = result.user;
-      console.log(genelprops)
-    }).catch(function (error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-    });
-  /*firebase.auth().getRedirectResult().then(function (result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-    }
-    var user = result.user;
-  }).catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    // ...
-  });*/
-  const setProfile = () => null;
+      console.log(genelprops);
+      return setUserObject(prev => ({ ...prev, name: displayName, imageUrl: photoURL, userId: uid }))
+    })
+      .catch(function (error) {
+        console.log(error)
+        /*var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;*/
+      })
+  };
+
+  const setProfile = (e) => {
+    e.preventDefault();
+    db.collection("profiles").doc(userObject.userId).set(userObject);
+  };
+  const inputHandle = (e) => setUserObject({ ...userObject, [e.target.id]: e.target.value });
   return (
     <Form>
       <Form.Row>
@@ -43,16 +50,33 @@ const SignUpPage = () => {
           <Button onClick={signViaGoogle}>Login</Button>
         </Col>
         <Col xl={2}>
-          <Form.Control placeholder="City" />
+          <Form.Control id='city' value={userObject.city} ref={input_city}
+            onChange={inputHandle}
+            placeholder="City" />
         </Col>
         <Col xl={2}>
-          <Form.Control placeholder="Describe yourself briefly" />
+          <Form.Control id='profile' value={userObject.brief} ref={input_brief}
+            onChange={inputHandle} placeholder="Describe yourself briefly" />
         </Col>
         <Col xl={2}>
           <Button onClick={setProfile} >Set Your Profile</Button>
         </Col>
       </Form.Row>
+      {console.log(userObject)}
     </Form>);
 };
 
 export default SignUpPage;
+ /*firebase.auth().getRedirectResult().then(function (result) {
+if (result.credential) {
+// This gives you a Google Access Token. You can use it to access the Google API.
+var token = result.credential.accessToken;
+}
+var user = result.user;
+}).catch(function (error) {
+var errorCode = error.code;
+var errorMessage = error.message;
+var email = error.email;
+var credential = error.credential;
+// ...
+});*/
--- src/TheProvider.js
+++ src/TheProvider.js
@@ -4,16 +4,16 @@ export const FacebookContext = createContext();
 
 export const TheProvider = ({ children }) => {
 
-    const [userObject, setUserObject] = useState({
+    /*const [userObject, setUserObject] = useState({
         city: 'The value in the "city" form field',
         name: 'The name in Google for the account',
         userId: 'The unique user ID (available via Google. Please dont make a user ID yourself)',
         imageUrl: ' The Google profile picture of the image (available via Google)',
         profile: 'The value of the profile form field'
     });
-
+*/
     return (
-        <FacebookContext.Provider value={[userObject, setUserObject]}>
+        <FacebookContext.Provider>
             {children}
         </FacebookContext.Provider>
     )
